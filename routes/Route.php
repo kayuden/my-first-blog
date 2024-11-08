b0698f2b844a9960ea4b5d87a64e65b74bed1835
kayuden <kbartholomot@gmail.com>
2024-11-08 15:28:04 +0100

    Initialising the autoload and setting up the router

---
+++ routes/Route.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Router;
+
+class Route{
+    public $path;
+    public $action;
+    public $matches;
+
+    public function __construct($path, $action)
+    {
+        $this->path = trim($path, '/');
+        $this->action = $action;
+    }
+
+    public function matches(string $url) // $url = posts/1 et $this->path = posts/:id
+    {
+        $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path); // $path = posts/([^/]+)
+        $pathToMatch = "#^$path$#"; // "#^/posts/([^/]+)$#"
+
+        if (preg_match($pathToMatch, $url, $matches)) { // #^/posts/([^/]+)$#,/posts/1 -> ['posts/1', '42']
+            $this->matches = $matches;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function execute()
+    {
+        $params = explode('@', $this->action);
+        $controller = new $params[0]();
+        $method = $params[1];
+
+        return isset($this->matches[1]) ? $controller->$method($this->matches[1]) : $controller->$method();
+    }
+}
