ac2de2e4eead40e0790042767faa4c18dd47d52d
kayuden <kbartholomot@gmail.com>
2024-12-29 20:38:32 +0100

    Add UML diagrams

--- config/Config.php
+++ config/Config.php
@@ -1,6 +1,6 @@
 <?php
 //DB Connection constants
-define('DB_HOST',       'localhost');
-define('DB_NAME',       'my_first_blog');
-define('DB_USER',       'root');
-define('DB_PASSWORD',   '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'my_first_blog');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
